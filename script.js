77b0d84d3eb31a72d79173649b30c08abdae64c3
Digguin <160871166+Digguin@users.noreply.github.com>
2024-04-22 07:20:16 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function desenhaImagens(){
-    canvas = document.getElementById("canv");
-    ctx = canvas.getContext("2d");
-    img = new Image();
-    img.onload = function(){
-        ctx.drawImage(img,0,0);
-        ctx.drawImage(image,600,0,100,100);
-    }
-}
